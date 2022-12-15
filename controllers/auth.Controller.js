85a1059295c14dcf365f47d5121c1ccceb86f378
Shivanand <Shivanand0297@gmail.com>
2022-12-16 00:49:15 +0530

    auth: login controller added

--- controllers/auth.Controller.js
+++ controllers/auth.Controller.js
@@ -11,8 +11,8 @@ import cookieOptions from "../utils/cookieOptions"
  * @returns User object
  */
 
-export const signup = asyncHandler(async (res, res)=>{
-    try {
+export const signup = asyncHandler(async (res, res)=>{  
+        // note that we dont need trycatch because we handled it in asynchandler
         // take input from thr frontend
         const {name, email, password} = req.body;
         
@@ -40,17 +40,47 @@ export const signup = asyncHandler(async (res, res)=>{
         // thats why we are setting password to undefined
         user.password = undefined;
         res.cookie("token", token, cookieOptions)
-        res.status(200).json({
+        return res.status(200).json({
             success: true, 
             token,
             user
         })
+})
 
-    } catch (error) {
-        console.log("error in singup", error.message);
-        res.status(400).json({
-            success: false,
-            message: `error in singup controller ${error.message}`
+/**
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/login
+ * @description User login controller for logging user
+ * @parameters email, password
+ * @returns User object
+ */
+
+export const login = asyncHandler(async(req, res)=>{
+    // taking input from user
+    const {name, email} = req.body;
+
+    // validating the input
+    if(!name || !email){
+        throw new CustomError("Please fill email and password", 400)
+    }
+
+    // checkfor existing user
+    const user = await User.findOne({email}).select("+password")
+    if(!user){
+        throw new CustomError("Invalid credentials", 400)
+    }
+
+    // matching password
+    const isPasswordMatched = await user.comparePassword(password)
+    if(isPasswordMatched){
+        const token = user.getJwtToken()
+        user.password = undefined
+        res.cookie("token", token, cookieOptions)
+        return res.status(200).json({
+            success: true, 
+            token,
+            user
         })
     }
+    throw new CustomError("Invalid credentials -pass", 400)
 })
