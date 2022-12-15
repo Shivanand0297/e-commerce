b5f499fdcaa979c525665d9138fa6bdee0d45e33
Shivanand <Shivanand0297@gmail.com>
2022-12-15 22:57:24 +0530

    added middlewares

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
+
+
+
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+app.use(cors())
+// morgan logger
+app.use(morgan("tiny")) // it print infirmation about our api request, response on the console
+
+
+
 
 export default app
