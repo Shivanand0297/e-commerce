927565af6aad7eb7f8cdf70ebdbdca4af00116b1
Shivanand <Shivanand0297@gmail.com>
2023-01-08 18:27:39 +0530

    comment added

--- services/imageUploder.js
+++ services/imageUploder.js
@@ -3,15 +3,15 @@ import s3 from "../config/s3.config";
 export const s3FileUpload = async({ bucketName, key, body, contentType })=>{    //key is file unique id
     return await s3.upload({
         Bucket: bucketName,
-        Key: key,
-        Body: body,
+        Key: key,   // key is unique file names
+        Body: body, // body is file to be uploaded
         ContentType: contentType
     }).promise()
 }
 
 export const s3FileDelete = async({ bucketName, key })=>{
-    return await s3.deleteObject({
+    return await s3.deleteObject({  //aws treat every file as a object
         Bucket: bucketName,
         Key: key
-    }).promise()    // this will return a promise, that will be handled who so ever is calling this method
+    }).promise()    // this will return a promise, that will be handled who so ever is calling this method, the promise will return full url of the fule that we can store in the database as a document property
 }
