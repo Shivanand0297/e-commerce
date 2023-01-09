55ed2d5a1f1c78c3e5692336493c351fa6177159
Shivanand <Shivanand0297@gmail.com>
2023-01-09 19:57:47 +0530

    added razorpay config and controllers

--- config/index.js
+++ config/index.js
@@ -20,6 +20,8 @@ const config = {    //this will become one mega object in global state
     S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
     S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
     S3_REGION: process.env.S3_REGION,
+    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_SECRET: process.env.RAZORPAY_SECRET
 
 }
 
